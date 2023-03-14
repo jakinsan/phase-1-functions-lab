9bc5e51c1fe8bd1e8a4479a6f56a025cf7679041
James Akinsanya <j.akins24@gmail.com>
2023-03-14 17:46:13 -0400

    commit tree e97daf43957ee326861f04504a2dafd1e906e600

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    if(blocks > 42) {
+        return blocks - 42
+    } else {
+        return 42 - blocks
+    }
+//    return blocks > 42 ? blocks - 42 : 42 - blocks 
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264 ;
+}
+
+function distanceTravelledInFeet(a, b) {
+    if(b>a) {
+        return (b - a) * 264 
+    } else {
+        return (a - b) * 264 
+    }
+}
+
+function calculatesFarePrice(start, destination) { 
+    const distance = distanceTravelledInFeet(start, destination)
+    if( distance <= 400) {
+        return 0
+    } else if(distance > 400 && distance < 2000)  {
+        return (distance - 400 ) * .02
+    } else if(distance > 2000 && distance <= 2500) {
+        return 25
+    } else if(distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
